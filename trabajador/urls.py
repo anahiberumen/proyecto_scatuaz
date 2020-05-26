fcbd80b18807781b83f32e707a2f25d8e58b3723
Anahí Berumen Murillo <anahi.berumen.m@gmail.com>
2020-05-26 16:05:13 -0500

    primer commit

---
+++ trabajador/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.lista_trabajador, name='lista_trabajador'),
+    path('<int:id>', views.ver_trabajador, name='ver_trabajador'),
+    path('agregar', views.agregar_trabajador, name='agregar_trabajador'),
+    path('eliminar/<int:pk>', views.eliminar_trabajador,
+         name='eliminar_trabajador'),
+    path('modificar/<int:id>', views.modificar_trabajador,
+         name='modificar_trabajador'),
+]
